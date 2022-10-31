74f9067dc948d5ba3e04b45aa853de7dc4ed99f1
Safat Mobid <safatmobid@gmail.com>
2022-10-30 23:14:50 -0400

    Finished Rooms html page design, started Javascript

---
+++ scripts/overnight.js
@@ -0,0 +1,15 @@
+"use strict";
+
+window.onload = init;
+
+function init() 
+{
+    const calculateBtn = document.getElementById("calculateBtn");
+    calculateBtn.onclick = calculateBtnClick;
+}
+
+function calculateBtnClick() 
+{
+
+
+}
